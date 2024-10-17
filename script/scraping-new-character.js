600742c4dd26b6e8e410d6a74ec6eaab52a4ffaa
Takahiro Kato <dozo13189@gmail.com>
2024-10-17 22:32:02 +0900

    enhance: 20240930-add-data

--- script/scraping-new-character.js
+++ script/scraping-new-character.js
@@ -353,7 +353,7 @@ const getCharacterDetailInfo = async (character) => {
 };
 
 (async () => {
-  const characterId = "1101022";
+  const characterId = "1186006";
   await getCharacterIcon(characterId);
   const characterBaseInfo = await getCharacterBaseInfo(characterId);
   const characterDetailInfo = await getCharacterDetailInfo(characterBaseInfo);
