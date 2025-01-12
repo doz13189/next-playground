fadda56ae23610cc0e61060ce2a19e1181969f7c
Takahiro Kato <dozo13189@gmail.com>
2025-01-12 15:20:01 +0900

    enhance: 20250112-add-data

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <Heading>お知らせ</Heading>
         </Box>
         <Typography>以下のデータ追加に伴うアップデートを実施しました。</Typography>
-        {["1113011", "1111011"].map((id) => (
+        {["1161008", "1113011", "1111011"].map((id) => (
           <Box key={id} marginY={"2"}>
             <Suspense fallback={
               <Skeleton>
