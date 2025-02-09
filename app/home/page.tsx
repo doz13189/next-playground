d7c34214cd6f77a05e492cea7d6a4229aab56bef
Takahiro Kato <dozo13189@gmail.com>
2025-02-09 16:33:01 +0900

    enhance/20250209-add-data (#104)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <Heading>お知らせ</Heading>
         </Box>
         <Typography>以下のデータ追加に伴うアップデートを実施しました。</Typography>
-        {["1105016"].map((id) => (
+        {["1114005", "1105016"].map((id) => (
           <Box key={id} marginY={"2"}>
             <Suspense fallback={
               <Skeleton>
--- script/scraping-new-character.js
+++ script/scraping-new-character.js
@@ -353,7 +353,7 @@ const getCharacterDetailInfo = async (character) => {
 };
 
 (async () => {
-  const characterId = "1105016";
+  const characterId = "1114005";
   await getCharacterIcon(characterId);
   const characterBaseInfo = await getCharacterBaseInfo(characterId);
   const characterDetailInfo = await getCharacterDetailInfo(characterBaseInfo);
