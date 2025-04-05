c22e4ea596b4b5a930636188b2c3576a778677f1
Takahiro Kato <dozo13189@gmail.com>
2025-04-05 18:50:34 +0900

    enhance: add data (#115)

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -16,7 +16,7 @@ export default function Page() {
           <Heading>お知らせ</Heading>
         </Box>
         <Typography>以下のデータ追加に伴うアップデートを実施しました。</Typography>
-        {["1235002"].map((id) => (
+        {["1105017", "1235002"].map((id) => (
           <Box key={id} marginY={"2"}>
             <Suspense fallback={
               <Skeleton>
