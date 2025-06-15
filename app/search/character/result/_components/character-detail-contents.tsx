e22fe3a4984da1691212831d7f0e23a74eb1f76c
Takahiro Kato <dozo13189@gmail.com>
2025-06-15 16:56:47 +0900

    feat: add a status value to character detail information

--- app/search/character/result/_components/character-detail-contents.tsx
+++ app/search/character/result/_components/character-detail-contents.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { MainLayout } from "@/app/_components/main-layout";
+import { characters } from "@/app/_data/character/object";
 import type { CharacterSchema } from "@/app/_data/character/schema";
 import { Button } from "@/app/_parts/button";
 import { Heading } from "@/app/_parts/heading";
 import { Typography } from "@/app/_parts/typography";
-import { css } from "@/styled-system/css";
-import { Box, Flex } from "@/styled-system/jsx";
+import { Box, Flex, HStack } from "@/styled-system/jsx";
 import { type FC, useState } from "react";
 import type { z } from "zod";
 
@@ -30,77 +30,192 @@ const getCharacterContent = (
   }
 };
 
-const getCharacterLevelLabel = (
+const getCharacterStatus = (
   activeTabState: number,
+  character: z.infer<typeof CharacterSchema>,
 ) => {
   switch (activeTabState) {
     case 0:
-      return "Lv.1";
+      return character.stats[0];
     case 1:
-      return "Lv.2";
+      return character.stats[1];
     case 2:
-      return "Lv.3";
+      return character.stats[2];
     case 3:
-      return "DX Lv.1";
-    case 4:
-      return "DX Lv.2";
+      return character.stats[3];
     default:
-      return "DX Lv.2";
+      return character.stats[3];
   }
 };
 
-const linkStyle = (isActive: boolean) => css({
-  width: "full",
-  fontSize: 'xs',
-  margin: '1',
-  paddingX: '1',
-  display: 'flex',
-  justifyContent: 'center',
-  borderWidth: '2px',
-  borderRadius: 'lg',
-  backgroundColor: isActive ? 'primary' : "secondary",
-  color: isActive ? 'secondary' : "primary",
-  borderColor: 'primary',
-});
+const getValueRank = ({ type, rarity, activeTabState, targetValue }: { type: "hp" | "power" | "speed", rarity: string, activeTabState: number, targetValue: string }) => {
+
+  const values = characters.characters.flatMap((character) => {
+    if (character.rarity === rarity) {
+      return Number(character.stats[activeTabState][type])
+    }
+
+    return [];
+  });
+
+  const sortedValues = values.sort((a, b) => a - b);
+  const rank = sortedValues.filter(value => value < Number(targetValue)).length;
+  const percentile = (rank / values.length) * 100;
+
+  if (percentile >= 75) {
+    return "S";
+  }
+
+  if (percentile >= 50) {
+    return "A";
+  }
+
+  if (percentile >= 25) {
+    return "B";
+  }
+
+  return "C";
+}
+
 
 export const CharacterDetailContents: FC<{
   character: z.infer<typeof CharacterSchema>;
 }> = ({ character }) => {
-  const [activeTabState, setActiveTabState] = useState<0 | 1 | 2 | 3 | 4>(4);
+  const [skillActiveTabState, setSkillActiveTabState] = useState<0 | 1 | 2 | 3 | 4>(4);
+  const [statusActiveTabState, setStatusActiveTabState] = useState<0 | 1 | 2 | 3>(3);
 
   return (
     <Box>
 
       <MainLayout>
-        <Heading>詳細</Heading>
+        <Heading>ステータス値</Heading>
         <Flex justifyContent={"center"} marginY={"4"} gap={"1"}>
           <Button
-            onClick={() => setActiveTabState(0)}
-            active={activeTabState === 0}
+            onClick={() => setStatusActiveTabState(0)}
+            active={statusActiveTabState === 0}
           >
             Lv.1
           </Button>
           <Button
-            onClick={() => setActiveTabState(1)}
-            active={activeTabState === 1}
+            onClick={() => setStatusActiveTabState(1)}
+            active={statusActiveTabState === 1}
+          >
+            {character.rarity === "r" ? "Lv.60" : character.rarity === "sr" ? "Lv.70" : "Lv.80"}
+          </Button>
+          <Button
+            onClick={() => setStatusActiveTabState(2)}
+            active={statusActiveTabState === 2}
+          >
+            Lv.MAX
+          </Button>
+          <Button
+            onClick={() => setStatusActiveTabState(3)}
+            active={statusActiveTabState === 3}
+          >
+            Adv+
+          </Button>
+        </Flex>
+
+        <Box marginBottom={"4"}>
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"HP"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getCharacterStatus(statusActiveTabState, character).hp}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "hp",
+                rarity: character.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue: getCharacterStatus(statusActiveTabState, character).hp
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"パワー"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getCharacterStatus(statusActiveTabState, character).power}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "power",
+                rarity: character.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue: getCharacterStatus(statusActiveTabState, character).power
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"スピード"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getCharacterStatus(statusActiveTabState, character).speed}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "speed",
+                rarity: character.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue: getCharacterStatus(statusActiveTabState, character).speed
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"クリティカル"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getCharacterStatus(statusActiveTabState, character).critical}`}
+            </Typography>
+          </HStack>
+
+          <Box marginTop={"2"}>
+            <Typography fontSize={"xs"}>
+              {"評価はS,A,B,Cの4つで、Sは上位25%、Aは上位25%〜50%、Bは上位50%〜75%、Cは下位25%です。また、評価は同じレアリティ、かつ、同じレベルのキャラクターの中で比較をしています。"}
+            </Typography>
+          </Box>
+
+        </Box>
+      </MainLayout>
+
+      <MainLayout>
+        <Heading>スキル詳細</Heading>
+        <Flex justifyContent={"center"} marginY={"4"} gap={"1"}>
+          <Button
+            onClick={() => setSkillActiveTabState(0)}
+            active={skillActiveTabState === 0}
+          >
+            Lv.1
+          </Button>
+          <Button
+            onClick={() => setSkillActiveTabState(1)}
+            active={skillActiveTabState === 1}
           >
             Lv.2
           </Button>
           <Button
-            onClick={() => setActiveTabState(2)}
-            active={activeTabState === 2}
+            onClick={() => setSkillActiveTabState(2)}
+            active={skillActiveTabState === 2}
           >
             Lv.3
           </Button>
           <Button
-            onClick={() => setActiveTabState(3)}
-            active={activeTabState === 3}
+            onClick={() => setSkillActiveTabState(3)}
+            active={skillActiveTabState === 3}
           >
             DX Lv.1
           </Button>
           <Button
-            onClick={() => setActiveTabState(4)}
-            active={activeTabState === 4}
+            onClick={() => setSkillActiveTabState(4)}
+            active={skillActiveTabState === 4}
           >
             DX Lv.2
           </Button>
@@ -109,14 +224,14 @@ export const CharacterDetailContents: FC<{
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
             <Typography color={"primary"}>
-              {`プルスウルトラ技（${getCharacterContent(activeTabState, character).plusUltra.name}）`}
+              {`プルスウルトラ技（${getCharacterContent(skillActiveTabState, character).plusUltra.name}）`}
             </Typography>
           </Box>
 
           <Typography
             // biome-ignore lint/security/noDangerouslySetInnerHtml:
             dangerouslySetInnerHTML={{
-              __html: getCharacterContent(activeTabState, character).plusUltra
+              __html: getCharacterContent(skillActiveTabState, character).plusUltra
                 .description,
             }}
           />
@@ -125,18 +240,18 @@ export const CharacterDetailContents: FC<{
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
             <Typography color={"primary"}>
-              {`アクションスキル（${getCharacterContent(activeTabState, character).actionSkill1.name}）`}
+              {`アクションスキル（${getCharacterContent(skillActiveTabState, character).actionSkill1.name}）`}
             </Typography>
           </Box>
 
           <Typography>
-            {`クールタイム ${getCharacterContent(activeTabState, character).actionSkill1.coolTime} ターン`}
+            {`クールタイム ${getCharacterContent(skillActiveTabState, character).actionSkill1.coolTime} ターン`}
           </Typography>
 
           <Typography
             // biome-ignore lint/security/noDangerouslySetInnerHtml:
             dangerouslySetInnerHTML={{
-              __html: getCharacterContent(activeTabState, character).actionSkill1
+              __html: getCharacterContent(skillActiveTabState, character).actionSkill1
                 .description,
             }}
           />
@@ -145,18 +260,18 @@ export const CharacterDetailContents: FC<{
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
             <Typography color={"primary"}>
-              {`アクションスキル（${getCharacterContent(activeTabState, character).actionSkill2.name}）`}
+              {`アクションスキル（${getCharacterContent(skillActiveTabState, character).actionSkill2.name}）`}
             </Typography>
           </Box>
 
           <Typography >
-            {`クールタイム ${getCharacterContent(activeTabState, character).actionSkill2.coolTime} ターン`}
+            {`クールタイム ${getCharacterContent(skillActiveTabState, character).actionSkill2.coolTime} ターン`}
           </Typography>
 
           <Typography
             // biome-ignore lint/security/noDangerouslySetInnerHtml:
             dangerouslySetInnerHTML={{
-              __html: getCharacterContent(activeTabState, character).actionSkill2
+              __html: getCharacterContent(skillActiveTabState, character).actionSkill2
                 .description,
             }}
           />
@@ -165,14 +280,14 @@ export const CharacterDetailContents: FC<{
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
             <Typography color={"primary"}>
-              {`オートスキル（${getCharacterContent(activeTabState, character).autoSkill1.name}）`}
+              {`オートスキル（${getCharacterContent(skillActiveTabState, character).autoSkill1.name}）`}
             </Typography>
           </Box>
 
           <Typography
             // biome-ignore lint/security/noDangerouslySetInnerHtml:
             dangerouslySetInnerHTML={{
-              __html: getCharacterContent(activeTabState, character).autoSkill1
+              __html: getCharacterContent(skillActiveTabState, character).autoSkill1
                 .description,
             }}
           />
@@ -181,13 +296,13 @@ export const CharacterDetailContents: FC<{
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
             <Typography color={"primary"}>
-              {`オートスキル（${getCharacterContent(activeTabState, character).autoSkill2.name}）`}
+              {`オートスキル（${getCharacterContent(skillActiveTabState, character).autoSkill2.name}）`}
             </Typography>
           </Box>
           <Typography
             // biome-ignore lint/security/noDangerouslySetInnerHtml:
             dangerouslySetInnerHTML={{
-              __html: getCharacterContent(activeTabState, character).autoSkill2
+              __html: getCharacterContent(skillActiveTabState, character).autoSkill2
                 .description
             }}
           />
--- app/search/memory/result/_components/memory-detail-contents.tsx
+++ app/search/memory/result/_components/memory-detail-contents.tsx
@@ -1,18 +1,22 @@
 "use client";
 
 import { MainLayout } from "@/app/_components/main-layout";
+import { memories } from "@/app/_data/memory/object";
 import type { MemorySchema } from "@/app/_data/memory/schema";
 import { Button } from "@/app/_parts/button";
 import { Heading } from "@/app/_parts/heading";
 import { Typography } from "@/app/_parts/typography";
 import { css } from "@/styled-system/css";
-import { Box, Flex } from "@/styled-system/jsx";
+import { Box, Flex, HStack } from "@/styled-system/jsx";
 import Image from "next/image";
 import { type FC, useState } from "react";
 import type { z } from "zod";
 
+type SkillActiveTabState = 0 | 1 | 2 | 3
+type StatusActiveTabState = 0 | 4 | 5
+
 const getMemoryContent = (
-  activeTabState: number,
+  activeTabState: SkillActiveTabState,
   memory: z.infer<typeof MemorySchema>,
 ) => {
   switch (activeTabState) {
@@ -29,65 +33,237 @@ const getMemoryContent = (
   }
 };
 
-const getMemoryLevelLabel = (
-  activeTabState: number,
+const getMemoryStatus = (
+  activeTabState: StatusActiveTabState,
+  memory: z.infer<typeof MemorySchema>,
+) => {
+
+  switch (activeTabState) {
+    case 0:
+      return memory.stats[0];
+    case 4:
+      return memory.stats[4];
+    case 5:
+      return memory.stats[5];
+    default:
+      return memory.stats[5];
+  }
+};
+
+const getValueRank = ({ type, rarity, activeTabState, targetValue }: { type: "hp" | "power" | "speed", rarity: string, activeTabState: StatusActiveTabState, targetValue: string }) => {
+
+  const values = memories.memories.flatMap((memory) => {
+    if (memory.rarity === rarity) {
+      return Number(memory.stats[activeTabState][type])
+    }
+
+    return [];
+  });
+
+  const sortedValues = values.sort((a, b) => a - b);
+  const rank = sortedValues.filter(value => value < Number(targetValue)).length;
+  const percentile = (rank / values.length) * 100;
+
+  if (percentile >= 75) {
+    return "S";
+  }
+
+  if (percentile >= 50) {
+    return "A";
+  }
+
+  if (percentile >= 25) {
+    return "B";
+  }
+
+  return "C";
+}
+
+
+const getCharacterLevelLabel = (
+  activeTabState: StatusActiveTabState,
+  rarity: "n" | "r" | "sr" | "ur",
 ) => {
   switch (activeTabState) {
     case 0:
-      return "Lv.1";
-    case 1:
-      return "Lv.2";
-    case 2:
-      return "Lv.3";
-    case 3:
-      return "DX Lv.1";
+      switch (rarity) {
+        case "n":
+          return "Lv.50";
+        case "r":
+          return "Lv.60";
+        case "sr":
+          return "Lv.70";
+        case "ur":
+          return "Lv.80";
+        default:
+          return "Lv.80";
+      }
+    case 4:
+      switch (rarity) {
+        case "n":
+          return "Lv.70";
+        case "r":
+          return "Lv.80";
+        case "sr":
+          return "Lv.90";
+        case "ur":
+          return "Lv.100";
+        default:
+          return "Lv.100";
+      }
+    case 5:
+      switch (rarity) {
+        case "n":
+          return "--";
+        case "r":
+          return "--";
+        case "sr":
+          return "Lv.95";
+        case "ur":
+          return "Lv.105";
+        default:
+          return "Lv.105";
+      }
     default:
-      return "DX Lv.1";
+      return "--";
   }
 };
 
 export const MemoryDetailContents: FC<{
   memory: z.infer<typeof MemorySchema>;
 }> = ({ memory }) => {
-  const initialActiveTabState = memory.rarity === "r" || memory.rarity === "n" ? 2 : 3;
-  const [activeTabState, setActiveTabState] = useState<0 | 1 | 2 | 3>(
-    initialActiveTabState,
+  const initialSkillActiveTabState = memory.rarity === "r" || memory.rarity === "n" ? 2 : 3;
+  const [skillActiveTabState, setSkillActiveTabState] = useState<SkillActiveTabState>(
+    initialSkillActiveTabState,
   );
+
+  const initialStatusActiveTabState = memory.rarity === "r" || memory.rarity === "n" ? 4 : 5;
+  const [statusActiveTabState, setStatusActiveTabState] = useState<StatusActiveTabState>(initialStatusActiveTabState);
 
 
   return (
     <Box>
+
       <MainLayout>
-        <Heading>詳細</Heading>
+        <Heading>ステータス値</Heading>
+        <Flex justifyContent={"center"} marginY={"4"} gap={"1"}>
+          <Button
+            onClick={() => setStatusActiveTabState(0)}
+            active={statusActiveTabState === 0}
+          >
+            {getCharacterLevelLabel(0, memory.rarity)}
+          </Button>
+          <Button
+            onClick={() => setStatusActiveTabState(4)}
+            active={statusActiveTabState === 4}
+          >
+            {getCharacterLevelLabel(4, memory.rarity)}
+          </Button>
+          {memory.rarity === "r" || memory.rarity === "n" ? null : (
+            <Button
+              onClick={() => setStatusActiveTabState(5)}
+              active={statusActiveTabState === 5}
+            >
+              {getCharacterLevelLabel(5, memory.rarity)}
+            </Button>
+          )}
+
+        </Flex>
+
+        <Box marginBottom={"4"}>
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"HP"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getMemoryStatus(statusActiveTabState, memory).hp}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "hp",
+                rarity: memory.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue:
+                  getMemoryStatus(statusActiveTabState, memory).hp
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"パワー"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getMemoryStatus(statusActiveTabState, memory).power}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "power",
+                rarity: memory.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue:
+                  getMemoryStatus(statusActiveTabState, memory).power
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <HStack marginBottom={"1"}>
+            <Typography>
+              {"スピード"}
+            </Typography>
+            <Typography color={"primary"}>
+              {`${getMemoryStatus(statusActiveTabState, memory).speed}`}
+            </Typography>
+            <Typography color={"tertiary"}>
+              {`（評価${getValueRank({
+                type: "speed",
+                rarity: memory.rarity,
+                activeTabState: statusActiveTabState,
+                targetValue:
+                  getMemoryStatus(statusActiveTabState, memory).speed
+              })}）`}
+            </Typography>
+          </HStack>
+
+          <Box marginTop={"2"}>
+            <Typography fontSize={"xs"}>
+              {"評価はS,A,B,Cの4つで、Sは上位25%、Aは上位25%〜50%、Bは上位50%〜75%、Cは下位25%です。また、評価は同じレアリティ、かつ、同じレベルのメモリーの中で比較をしています。"}
+            </Typography>
+          </Box>
+
+        </Box>
+      </MainLayout>
+
+      <MainLayout>
+        <Heading>スキル詳細</Heading>
         <Flex justifyContent={"center"} gap="1" marginY={"4"}>
           <Button
-            onClick={() => setActiveTabState(0)}
-            active={activeTabState === 0}
+            onClick={() => setSkillActiveTabState(0)}
+            active={skillActiveTabState === 0}
           >
             Lv.1
           </Button>
           <Button
-            onClick={() => setActiveTabState(1)}
-            active={activeTabState === 1}
+            onClick={() => setSkillActiveTabState(1)}
+            active={skillActiveTabState === 1}
           >
             Lv.2
           </Button>
           <Button
-            onClick={() => setActiveTabState(2)}
-            active={activeTabState === 2}
+            onClick={() => setSkillActiveTabState(2)}
+            active={skillActiveTabState === 2}
           >
             Lv.3
           </Button>
           {memory.rarity === "r" || memory.rarity === "n" ? null : (
             <Button
-              onClick={() => setActiveTabState(3)}
-              active={activeTabState === 3}
+              onClick={() => setSkillActiveTabState(3)}
+              active={skillActiveTabState === 3}
             >
               DX Lv.1
             </Button>
           )}
         </Flex>
-
 
         <Box marginBottom={"4"}>
           <Box marginBottom={"1"}>
@@ -97,7 +273,7 @@ export const MemoryDetailContents: FC<{
           </Box>
 
           <Typography>
-            {getMemoryContent(activeTabState, memory)}
+            {getMemoryContent(skillActiveTabState, memory)}
           </Typography>
 
         </Box>
