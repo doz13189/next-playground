6a9c9c1bd30ed5854d901c03164e09f6f3b45f0d
Takahiro Kato <dozo13189@gmail.com>
2025-12-23 11:19:24 +0900

    otameshi (#147)
    * otameshi
    
    * chore: fix
    
    * chore: fix
    
    * chore: sample

---
+++ app/search/character/layout.tsx
@@ -0,0 +1,47 @@
+import type { Metadata } from "next";
+
+const serviceTitle = "test";
+const serviceDescription =
+  "テスト用の説明文です。";
+const serviceImageUrl =
+  "https://placehold.jp/150x150.png";
+const serviceUrl = "http://localhost";
+
+export const metadata: Metadata = {
+  metadataBase: new URL(serviceUrl),
+  title: serviceTitle,
+  description: serviceDescription,
+  openGraph: {
+    type: "website",
+    url: serviceUrl,
+    title: serviceTitle,
+    description: serviceDescription,
+    images: [
+      {
+        url: serviceImageUrl,
+        width: 800,
+        height: 600,
+        alt: "Og Image Alt",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary",
+    title: serviceTitle,
+    description: serviceDescription,
+    images: [
+      {
+        url: serviceImageUrl,
+        alt: "Twitter Image Alt",
+      },
+    ],
+  },
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
